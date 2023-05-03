52fed22a15850892df816237667b95dabefe14c1
Ilya Gulko <thankyou@mit.edu>
2023-05-03 04:57:23 -0400

    added hypercode theme

---
+++ template/js/desktop.js
@@ -0,0 +1,23 @@
+// desktop.js
+function DesktopOnLoad () {
+	//alert('DEBUG: DesktopOnLoad()');
+	if (window.location.href.indexOf('desktop') != -1) {
+		if (window.DraggingInit && window.DraggingMakeFit && window.SetActiveDialog && window.GetParentDialog && window.displayNotification) {
+			if (! GetPrefs('draggable')) {
+				SetPrefs('draggable', 1);
+				SetPrefs('draggable_activate', 1);
+				SetPrefs('draggable_spawn', 1);
+				SetPrefs('draggable_spawn_focus', 1);
+				SetPrefs('draggable', 1);
+				SetPrefs('draggable_restore', 1);
+				SetPrefs('draggable_restore_collapsed', 1);
+				SetActiveDialog(GetParentDialog(this));
+				DraggingInit(0);
+				DraggingMakeFit(0);
+				DraggingInit(0);
+				displayNotification('Annoying mode activated.');
+			}
+		}
+	}
+} // DesktopOnLoad()
+// / desktop.js
